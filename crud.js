ddf366b365c886a5d143badf92ac4c42be15ebd9
Yash <yashgadhwe@gmail.com>
2024-04-12 15:43:14 +0530

    all errors fixed

--- crud.js
+++ crud.js
@@ -30,30 +30,28 @@ function fetchData() {
 
 //Function to add data inside the table 
 
-function addData() {
+function addData(studentData) {
 
 	//target the tbody element
 	//(here first targated table element by its id selecotor
 	// and then, inside we need to select table body using tag its tag name)
 	//it just like nested selectors
-	let table = document
-		.getElementById('studentList')
-		.getElementsByTagName('tbody')[0];
+	let table = document.getElementById('studentList').getElementsByTagName('tbody')[0];
 
 	//it inserts the new row inside the tbody of size 0
-	let newRow = table.inserRow(0);
+	let newRow = table.insertRow(0);
 
 	cell1 = newRow.insertCell(0);
-	cell1.innerHTML = data.username;
+	cell1.innerHTML = studentData.username;
 
 	cell2 = newRow.insertCell(1);
-	cell2.innerHTML = data.email;
+	cell2.innerHTML = studentData.email;
 
 	cell3 = newRow.insertCell(2);
-	cell3.innerHTML = data.phoneno;
+	cell3.innerHTML = studentData.phoneno;
 
 	cell4 = newRow.insertCell(3);
-	cell4.innerHTML = data.city;
+	cell4.innerHTML = studentData.city;
 
 	cell5 = newRow.insertCell(4);
 	cell5.innerHTML = `<button onclick = "editData(this)"> EDIT </button>
@@ -71,11 +69,14 @@ function updateData(data) {
 }
 
 //delete function to delete the selected row
-
-if (confirm("Are you sure that, You want to delete the data")) {
-	row = btn.parentElement.parentElement;
-	document.getElementById('studentList').deleteRow(row.rowIndex)
+function deleteData(btn) {
+    if (confirm("Are you sure that, You want to delete the data")) {
+        row = btn.parentElement.parentElement;
+        document.getElementById('studentList').deleteRow(row.rowIndex);
+    }
 }
+
+
 
 //edit functionality
 
